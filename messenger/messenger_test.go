5ec04810b1ed56c4eb11b0468d0864bd5ed86d98
Rusty Eddy <rusty@rustyeddy.com>
2026-01-02 17:27:12 +0000

    test(messenger): consolidate SubscribeAll tests into messenger_test.go

--- messenger/messenger_test.go
+++ messenger/messenger_test.go
@@ -2,6 +2,7 @@ package messenger
 
 import (
 	"context"
+	"fmt"
 	"os"
 
 	"testing"
@@ -82,7 +83,7 @@ func TestSetMessengerNil(t *testing.T) {
 	assert.Nil(t, GetMessenger())
 }
 
-func TestMessengerConnect_None(t *testing.T) {
+func TestMessengerConnectNone(t *testing.T) {
 	orig := GetMessenger()
 	defer SetMessenger(orig)
 
@@ -94,7 +95,7 @@ func TestMessengerConnect_None(t *testing.T) {
 	assert.NoError(t, m.Connect())
 }
 
-func TestMessengerClose_NoPanic(t *testing.T) {
+func TestMessengerCloseNoPanic(t *testing.T) {
 	orig := GetMessenger()
 	defer SetMessenger(orig)
 
@@ -110,4 +111,59 @@ func TestMessengerClose_NoPanic(t *testing.T) {
 
 	// Close should call Unsub on the connection and not panic
 	m.Close()
+}
+
+type mockConn struct {
+	subs   []string
+	subErr map[string]error
+}
+
+func (mc *mockConn) Connect(b string, u string, p string) error { return nil }
+func (mc *mockConn) Close()                                     {}
+func (mc *mockConn) Sub(topic string, handler MsgHandler) error {
+	mc.subs = append(mc.subs, topic)
+	if mc.subErr != nil {
+		if err, ok := mc.subErr[topic]; ok {
+			return err
+		}
+	}
+	return nil
+}
+func (mc *mockConn) Unsub(topics ...string) {}
+func (mc *mockConn) PubMsg(m *Msg) error    { return nil }
+func (mc *mockConn) IsConnected() bool      { return true }
+
+func TestSubscribeAllNoSubscriptions(t *testing.T) {
+	m := NewMessenger("none")
+	assert.NotNil(t, m)
+	if m == nil {
+		return
+	}
+	m.subscriptions = map[string]MsgHandler{} // ensure empty
+	mock := &mockConn{}
+	m.Conn = mock
+
+	m.SubscribeAll(nil)
+
+	assert.Len(t, mock.subs, 0)
+}
+
+func TestSubscribeAllWithSubscriptionsAndErrors(t *testing.T) {
+	m := NewMessenger("none")
+	assert.NotNil(t, m)
+	if m == nil {
+		return
+	}
+	mock := &mockConn{subErr: map[string]error{"bad/topic": fmt.Errorf("fail")}}
+	m.Conn = mock
+
+	m.subscriptions = map[string]MsgHandler{
+		"good/topic": func(*Msg) error { return nil },
+		"bad/topic":  func(*Msg) error { return nil },
+	}
+
+	m.SubscribeAll(nil)
+
+	assert.Len(t, mock.subs, 2)
+	assert.ElementsMatch(t, []string{"good/topic", "bad/topic"}, mock.subs)
 }
